a0c38df855b17ed9ec521868a2a8a91caf2d8946
bogdansharp <bogdansharpy@gmail.com>
2025-11-21 23:20:56 +0000

    CsvParser tracks position to enchance exception messages

---
+++ src/test/java/com/group5/csv/exceptions/ParseExceptionTest.java
@@ -0,0 +1,67 @@
+package com.group5.csv.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParseExceptionTest {
+
+    @Test
+    void setsMessageAndCause() {
+        String message = "Parsing failed";
+        Throwable cause = new IllegalArgumentException("Invalid input");
+        ParseException ex = new ParseException(message, cause);
+
+        assertEquals(message, ex.getMessage());
+        assertEquals(cause, ex.getCause());
+    }
+
+    @Test
+    void getMessageCorrectly() {
+        String msg = "Parsing failed";
+        ParseException ex = new ParseException(msg);
+        assertEquals(msg, ex.getMessage());
+    }
+
+    @Test
+    void getLineInMessage() {
+        String msg = "Parsing failed";
+        ParseException ex = new ParseException(msg, 1_000_000_007, -4);
+        assertTrue(ex.getMessage().toLowerCase().contains("line")
+                && ex.getMessage().toLowerCase().contains("" + 1_000_000_007));
+    }
+
+    @Test
+    void getColInMessage() {
+        String msg = "Parsing failed";
+        ParseException ex = new ParseException(msg, 1, 1_000_000_007);
+        assertTrue(ex.getMessage().toLowerCase().contains("col")
+                && ex.getMessage().toLowerCase().contains("" + 1_000_000_007));
+    }
+
+    @Test
+    void getLineCorrectly() {
+        ParseException ex = new ParseException("Parsing failed", 1_000_000_007, 0);
+        assertEquals(1_000_000_007, ex.getLine());
+    }
+
+    @Test
+    void setLineCorrectly() {
+        ParseException ex = new ParseException("Parsing failed", -1, 0);
+        ex.setLine(1_000_000_007);
+        assertEquals(1_000_000_007, ex.getLine());
+    }
+
+    @Test
+    void getColumnCorrectly() {
+        ParseException ex = new ParseException("Parsing failed", 0, 1_000_000_007);
+        assertEquals(1_000_000_007, ex.getColumn());
+    }
+
+    @Test
+    void setColumnCorrectly() {
+        ParseException ex = new ParseException("Parsing failed", -1, -1);
+        ex.setColumn(1_000_000_007);
+        assertEquals(1_000_000_007, ex.getColumn());
+    }
+}
